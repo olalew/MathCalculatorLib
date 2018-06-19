ed7ff342bb5e14701b24ddfd5b2188ae2902786d
Ola Lewandowska <ola.lewandowska12@gmail.com>
2018-06-19 20:36:17 +0200

    del

--- dist/mathcalc.d.ts
+++
@@ -1,5 +0,0 @@
-import { IonicNativePlugin } from '@ionic-native/core';
-import { Observable } from 'rxjs';
-export declare class MathProvider extends IonicNativePlugin {
-    add(arg1: any): Observable<any>;
-}
