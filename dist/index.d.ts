9fe9624e947999dcbbb6940b7fa62bfcc1a5cd0f
Ola Lewandowska <ola.lewandowska12@gmail.com>
2018-06-19 20:32:31 +0200

    'commit'

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { IonicNativePlugin } from '@ionic-native/core';
+import { Observable } from 'rxjs';
+export declare class MathProvider extends IonicNativePlugin {
+    add(arg1: any): Observable<any>;
+}
